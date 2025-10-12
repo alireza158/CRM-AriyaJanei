5178fc2e7501eb9532c7dc56c38b8d1e121482bd
unknown <aryajanebi4@gmail.com>
2025-10-12 15:08:06 +0330

    V2.6 fix admin notes

--- resources/views/admin/customers/index.blade.php
+++ resources/views/admin/customers/index.blade.php
@@ -188,7 +188,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 <!-- Bootstrap CSS -->
 
-
+ <script>
 document.addEventListener('DOMContentLoaded', function() {
     const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
